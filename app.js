e2155f2c3f79c4cad65599b9545890a1bafcff24
Asima Abid <asimaabid124@gmail.com>
2020-06-10 20:32:47 +0500

    assignmnet

---
+++ app.js
@@ -0,0 +1,39 @@
+// alert("Hello Welcome to my website");
+// alert("Error!Please enter a valid password");
+// alert("Welcome to JS land....\n Happy Coding");
+// alert("Welcome to JS land....");
+
+
+// var a=10;
+// var b=10;
+// var c=a+b;
+// console.log(c);
+// var username;
+// var myname="Asima Abid";
+// alert(myname);
+// var message;
+// message="hello world";
+// alert(message);
+// var name="John Doe";
+// var age="15 years old";
+// var course="certified mobile application development";
+// alert(name);
+// alert(age);
+// alert(course);
+ 
+// var x=("pizza\npizz \npiz \npi  \np");
+// alert(x);
+// var book="A smarter way to learn JavaScript ";
+// alert("i am trying to learn from the book "+book);
+
+// var email="asima@gmail.com";
+// alert("my email is "+email);
+
+// var strmg="----"
+
+// var age=19;
+// alert("your age is "+age+"year old");
+
+var birthyear="2001";
+alert("Your birth year is "+birthyear+"\n data type of your decleared variable is number");
+ 
