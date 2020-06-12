660302b8a2cb52e5512a8673c813d5659d87089b
Asima Abid <asimaabid124@gmail.com>
2020-06-13 02:50:27 +0500

    working

--- app.js
+++ app.js
@@ -34,6 +34,329 @@
 // var age=19;
 // alert("your age is "+age+"year old");
 
-var birthyear="2001";
-alert("Your birth year is "+birthyear+"\n data type of your decleared variable is number");
+// var birthyear="2001";
+// alert("Your birth year is "+birthyear+"\n data type of your decleared variable is number");
+//  document.write("Asima");
+
+//  var yourName, yourAge, yourEmail;
+//  var _name,age1,myAlert,twoVar,time;
+//  var #name,age^,my*age,var,alert;
+
+//  document.write("Variable names can only contain number,$ and _ For example $my_1stVariable \n
+//  Variables must begin with a numer,letter or _ \n For example $name, _name or name \n
+//   Variable names are case senstitive \n
+//    Variable names should not be JS keyword");
+
+//    var num1= +prompt("Enter a number");
+//    var num2= +prompt("Enter number");
+//    num3=num1+num2;
+// document.write("Sum of "+num1+" and "+num2+"is "+num3);
+// num4=num1-num2;
+// document.write("Subtraction of "+num1+" and "+num2+"is "+num4);
+// num5=num1/num2;
+// document.write("Division of "+num1+" and "+num2+"is "+num5);
+// num6=num1%num2;
+// document.write("Modulus of "+num1+" and "+num2+"is "+num6);
+
+// var a;
+// document.write("Value after declaration is:"+a +"\n a");
+// var a=10;
+// document.write("initial value:"+a);
+// a++
+// document.write("After increament:"+a);
+// a=a+7;
+// document.write("Value after addition is:"+a);
+// a--
+// document.write("Value after decreament is:"+a);
+// a=a/3
+// document.write(" Value after reaminder is:"+a);
+
+// var ticketPrice=600;
+// totalCost=ticketPrice*5;
+// document.write("Total Cost to buy 5 tickets to a movie is"+totalCost+"PKR");
+
+
+// for(a=1;a<10;a++)
+// {
+//    document.write("4*"+a+"="+a*4);
+//    document.write("<br>");
+// }
+
+// var celciusTemp=25;
+// ConTemperature=(celciusTemp-32)*(5/9);
+// document.write(celciusTemp+"is "+ConTemperature);
+// var CelciusTemp=70;
+// ferTemperature=(CelciusTemp*(9/5))+32;
+// document.write(ferTemperature)
+
+
+
+
+
+
+
+// NO#6
+// var a=10;
+// document.write(a)
+// document.write("<br>")
+// ++a;
+// document.write("The value of ++a is :"+a )
+// document.write("<br>")
+// document.write("Now the value of a is :"+a)
+// document.write("<br>")
+// a++;
+// document.write("The value of a++ is :"+a )
+// document.write("<br>")
+// document.write("Now the value of a is :"+a)
+// document.write("<br>")
+// a--;
+// document.write("The value of a-- is :"+a )
+// document.write("<br>")
+// a--;
+// document.write("Now the value of a is :"+a)
+
+
+// Q#3 Write a program that takes input a name from user &greet the user.
+// var x=prompt("Enter your name");
+// alert("Welcome to this page "+x);
+
+// Q#55. Write a program to take input a number from user &
+// display it’s multiplication table on your browser. If user
+// does not enter a new number, multiplication table of 5
+// should be displayed by default.
+
+// var table= prompt("Enter a number");
+// if (table>0) {
+//     for(a=1;a<=10;a++)
+// {
+//    document.write(table+"*"+a+"="+table*a);
+//     document.write("<br>");
+//  }
+// }
+// else {
+//  for(b=1;b<=10;b++)
+//  {
+//     document.write("5*"+b+"="+5*b);
+//     document.write("<br>");
+//  }
+// }
+
+
+// Q6) Take three subjects name from user and store them in 3
+// different variables.
+// var sub1=prompt("Enter name of first subject");
+// var sub2=prompt("Enter name of second subject");
+// var sub3=prompt("Enter name of third subject");
+// var total=100;
+// var nums1=prompt("enter the number of "+sub1);
+// var nums2=prompt("enter the number of "+sub2);
+// var nums3=prompt("enter the number of "+sub3);
+// var per=(num1*total)/100;
+// document.write(sub1+total+num1+per);
+
+// chapter 9 to 12
+// var a=prompt("Enter city name:");
+// if (a=="Karachi")
+// {
+//   alert("Welcome to city of lights");
+// }
+
+// var gender=prompt("Enter your gender please");
+// if(gender=="male")
+// {
+//  document.write("Good morning Sir");
+
+// }
+// else
+// {
+//    document.write("Good morning Ma'am");
+   
+// }
+
+// var color=prompt("Please enter the signal color");
+// if(color=="Red"){
+//    document.write("\t Must Stop");
+// }
+// else if(color=="Yellow"){
+//    document.write("Ready to move");
+// }
+// else
+// {
+//    document.write("Move now");
+// }
+
+// var fuel=prompt("Enter the remaining fuel in car(in litre)")
+// if(fuel<0.25)
+// {
+//    alert("Please refill fuel in your car");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+
+//  var sub1=prompt("Enter name of first subject");
+//  var sub2=prompt("Enter name of second subject");
+//  var sub3=prompt("Enter name of third subject");
+// //  var max_total=100;
+//  var sub_total=sub1+sub2+sub3;
+//  var per=(sub_total*100)/300;
  
+
+//  if(per>=80){
+//    document.write("Total marks:300");
+// //    document.write("<br>");
+// //   document.write(var sub_total);
+// //     document.write("A-One");
+
+
+//  }
+//  else if(per>=70){
+
+//    document.write("Good");
+
+// }
+// else if(per>=60){
+
+//    document.write("You need to improve");
+
+// }
+// else
+// {
+
+//    document.write("Sorry");
+
+// }
+
+// var number=prompt("Enter a number between 1 and 100");
+// var S_number=20;
+// num2=num1+1;
+//   if(number==S_number){
+//   document.write("Bingo.... Correct number");
+//   }
+  
+//   else if(num2==S_number){
+//    document.write("Close enough to coorect number");
+//    }
+//    else{
+//       document.write("@")
+//    }
+
+// var divNum=prompt("Enter a number");
+// if ((divNum%3)==0){
+//    alert("Number is divisible by 3");
+// }
+// 
+// var num1= +prompt("Enter the first number");
+// var num2= +prompt("Enter the second number");
+// var op=prompt("Enter a operator");
+// sum=num1+num2;
+// if(op=="+"){
+//    alert("Sum is "+sum);
+// }
+
+// var character=prompt("Please enter a character");
+// if(character>=65 && character<=90){
+//    alert("UpperCase");
+// }
+
+// var fNumber= +prompt("Enter the first number");
+// var sNumber= +prompt("Enter the second number");
+// if(fNumber>sNumber){
+//    alert("Number 1 is greater");
+
+// }
+// else if(sNumber>fNumber){
+//    alert("Number 2 is greater");
+   
+// }
+// else{
+//    alert("Both are equal");
+// }
+
+
+// var number= +prompt("Enter the number");
+// if(number>"0"){
+//    alert("Number is possitive");
+
+// }
+// else if(number<"0"){
+//    alert("Number is negative");
+   
+// }
+// else{
+//    alert("Number is zero");
+// }
+
+// var ch= prompt("Enter a character");
+// if(ch=="a" || ch=="e"|| ch=="i"|| ch=="0"|| ch=="u")
+// {
+//    alert("True.. its a vowel");
+// }
+// else("False it is not a vowel");
+
+// var password=prompt("Enter you password please");
+// if (password==0)
+// {
+//    alert("Please enter your password");
+// }
+// if (password=="Asima")
+// {
+//    alert("Correct! The password you entered matches with original");
+// }
+// else
+// {
+//    alert("Incorrect! The password you entered does not matches with original");
+// }
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// }
+// else
+// {
+// greeting = "Good evening";
+// }
+
+// var time=prompt("Enter the time:");
+// if(time==13){
+//    alert("1 PM")
+// }
+// else if(time==14){
+//    alert("2 PM")
+// }
+// else if(time==15){
+//    alert("3 PM")
+// }
+// else if(time==16){
+//    alert("4 PM")
+// }
+// else if(time==17){
+//    alert("5 PM")
+// }
+// else if(time==18){
+//    alert("6 PM")
+// }
+// else if(time==19){
+//    alert("7 PM")
+// }
+// else if(time==20){
+//    alert("8 PM")
+// }
+// else if(time==21){
+//    alert("9 PM")
+// }
+// else if(time==22){
+//    alert("10 PM")
+// }
+// else if(time==23){
+//    alert("11 PM")
+// }
+// else if(time==24){
+//    alert("12 PM")
+// }
+
+var 
